5018733e8e9e48260caf645a970d634be3bdb9dd
Iustin Vasiliu <iustinv2.0@gmail.com>
2023-04-03 17:31:53 +0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,25 @@
-import React from 'react';
-import {BrowserRouter, Route, Routes} from "react-router-dom";
-import './App.css';
+import { Footer } from "./components/Footer";
+import { Navigation } from "./components/Navigation";
+import { Feed } from "./pages/Feed";
 import Login from "./pages/Login";
 import {Register} from "./pages/Register";
-
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path={`/`} element={<Login/>}/>
-        <Route path="/login" element={<Login/>}/>
-        <Route path="/register" element={<Register/>}/>
-        <Route path="*" element={<Login/>}/>
-      </Routes>
-    </BrowserRouter>
+    <>
+      {localStorage.token ? <Navigation /> : <></>}
+      <BrowserRouter>
+        <Routes>
+          <Route path={`/`} element={<Feed />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="*" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+      {/*<Footer /> */}
+    </>
   );
 }
 
