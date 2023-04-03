fa424894f9837dff70de368efdfb2118d10f6d84
Iustin Vasiliu <iustinv2.0@gmail.com>
2023-04-03 16:00:03 +0300

    added server and login func

---
+++ server/server.js
@@ -0,0 +1,189 @@
+const express = require("express");
+const db = require("./db");
+const cors = require("cors");
+
+const app = express();
+const PORT = 3002;
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+app.use(cors());
+app.use(express.json());
+
+// simple route
+app.get("/api/users", (req, res) => {
+  db.query("SELECT * FROM users", (err, result) => {
+    if (err) {
+      console.log(err);
+      res.status(500).json({ error: "Error fetching data from database" });
+      return;
+    }
+    res.json(result);
+  });
+});
+
+app.get("/api/categories/:id/subcategories", (req, res) => {
+  const categoryId = req.params.id;
+
+  db.query(
+    "SELECT * FROM subcategorie WHERE id_categorie = ?",
+    categoryId,
+    (err, result) => {
+      if (err) {
+        console.log(err);
+        res.status(500).json({ error: "Error fetching data from database" });
+        return;
+      }
+      res.setHeader("Content-Type", "application/json");
+      res.json(result);
+    }
+  );
+});
+app.get("/api/listings/:id", (req, res) => {
+  const subcategoryId = req.params.id;
+
+  db.query(
+    "SELECT * FROM anunt WHERE id_subcategorie = ?",
+    subcategoryId,
+    (err, result) => {
+      if (err) {
+        console.log(err);
+        res.status(500).json({ error: "Error fetching data from database" });
+        return;
+      }
+      result.forEach((listing) => {
+        listing.imagine = Buffer.from(listing.imagine).toString("base64");
+      });
+      res.setHeader("Content-Type", "application/json");
+      res.json(result);
+    }
+  );
+});
+
+app.get("/api/search/:id", (req, res) => {
+  const searchTerm = req.params.id;
+  const query = `%${searchTerm}%`;
+  db.query(
+    "SELECT * FROM anunt WHERE titlu LIKE ? OR descriere LIKE ?",
+    [query, query],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+        res.status(500).json({ error: "Error fetching data from database" });
+        return;
+      }
+      result.forEach((listing) => {
+        listing.imagine = Buffer.from(listing.imagine).toString("base64");
+      });
+      res.setHeader("Content-Type", "application/json");
+      res.json(result);
+    }
+  );
+});
+
+app.post("/api/register", (req, res) => {
+  const { email, parola } = req.body;
+  const saltRounds = 10;
+  bcrypt.hash(parola, saltRounds, (err, hashedPassword) => {
+    if (err) {
+      console.log(err);
+      res.status(500).json({ error: "Error hashing password" });
+      return;
+    }
+    db.query(
+      "INSERT INTO users (email,parola) VALUES (?, ?)",
+      [email, hashedPassword],
+      (err, result) => {
+        if (err) {
+          console.log(err);
+          res.status(500).json({ error: "Error inserting user into database" });
+          return;
+        }
+        res.status(200).json({ message: "User registered successfully" });
+      }
+    );
+  });
+});
+
+app.post("/api/login", (req, res) => {
+  const { email, parola } = req.body;
+  db.query(
+    "SELECT * FROM users WHERE email = ?",
+    email,
+    (err, result) => {
+      if (err) {
+        console.log(err);
+        res
+          .status(500)
+          .json({ error: "Error fetching user data from database" });
+        return;
+      }
+      if (result.length === 0) {
+        res.status(401).json({ error: "Invalid credentials" });
+        return;
+      }
+      const user = result[0];
+      bcrypt.compare(parola, user.parola, (err, isMatch) => {
+        if (err) {
+          console.log(err);
+          res.status(500).json({ error: "Error comparing passwords" });
+          return;
+        }
+        if (!isMatch) {
+          res.status(401).json({ error: "Invalid credentials" });
+          return;
+        }
+        const payload = { id: user.id };
+        const options = { expiresIn: "1h" };
+        const secretKey = "secretkey";
+        jwt.sign(payload, secretKey, options, (err, token) => {
+          if (err) {
+            console.log(err);
+            res.status(500).json({ error: "Error signing token" });
+            return;
+          }
+          res.status(200).json({ token });
+        });
+      });
+    }
+  );
+});
+
+app.post("/api/anunt", (req, res) => {
+  const { titlu, descriere, data, id_subcategorie, id_user, imagine } =
+    req.body;
+  const base64Image = imagine.replace(/^data:image\/\w+;base64,/, "");
+  const binaryImage = Buffer.from(base64Image, "base64");
+  const query = `
+    INSERT INTO anunt (titlu, descriere, data, id_subcategorie, id_user, imagine)
+    VALUES (?, ?, ?, ?, ?, ?)
+  `;
+
+  const values = [
+    titlu,
+    descriere,
+    data,
+    id_subcategorie,
+    id_user,
+    binaryImage,
+  ];
+
+  db.query(query, values, (err, result) => {
+    if (err) {
+      console.error(err);
+      res
+        .status(500)
+        .json({ error: "Error inserting anunt into the database" });
+      return;
+    }
+
+    res
+      .status(201)
+      .json({ message: "Anunt created successfully", id: result.insertId });
+  });
+});
+
+// set port, listen for requests
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`);
+});
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,64 +1,142 @@
-import React, { useContext, useState } from "react";
-import { useNavigate } from "react-router-dom";
-
-import styled from "styled-components";
-
-import "./login.css";
-import redEclipseLogo from "../images/RedEclipse.webp"
+import redEclipseLogo from "../images/RedEclipse.webp";
+import Axios from "axios";
+import React from "react";
 
 const Login = () => {
-    return (
+  const [credentials, setCredentials] = React.useState({
+    email: "",
+    password: "",
+  });
+
+  const [errorState, setErrorState] = React.useState("");
+  const handleRegister = () => {
+    Axios.post("http://localhost:3002/api/register", {
+      email: "iustinboss@test.ro",
+      parola: "parola",
+    })
+      .then((response) => {
+        console.log(response.data.message);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  // React.useEffect(() => {
+  //   if (localStorage.token) {
+  //     window.location.href = `/acasa`;
+  //   }
+  // }, []);
+  const handleLogin = (email: string, parola: string) => {
+    console.log(email, parola);
+    Axios.post("http://localhost:3002/api/login", {
+      email: email,
+      parola: parola,
+    })
+      .then((response) => {
+        console.log(response.data.message);
+        localStorage.setItem("token", response.data.token);
+        setErrorState("");
+        window.location.href = `acasa`;
+      })
+      .catch((error) => {
+        setErrorState(error);
+      });
+  };
+
+  const handleChange = (e: any) => {
+    setCredentials((prev) => ({ ...prev, [e.target.id]: e.target.value }));
+  };
+
+  return (
+    <>
+      {localStorage.token ? (
         <div>
-            <div className="bg-white pt-6 sm:pt-8 lg:pt-12">
-                <div className="mx-auto max-w-screen-2xl px-4 md:px-8">
-
-                    <div className="flex justify-center h-16 mb-6 md:h-32">
-                            <img
-                                src={redEclipseLogo}
-                                loading="lazy" alt="Red Eclipse logo"
-                                className="absolute w-64 object-cover object-center mx-auto md:w-96"/>
-                    </div>
-                    <div className=" my-4 md:my-0">
-                        <p className="mx-auto max-w-screen-md text-center text-black md:text-lg">Bun venit pe RedEclipse - locul unde pasiunile, interesele și ideile tale se întâlnesc cu o comunitate vibrantă și plină de energie. Fie că ești aici pentru a-ți conecta cu prietenii și familia, pentru a-ți extinde rețeaua de contacte sau pentru a explora comunități și culturi noi, suntem încântați să te avem alături de noi. </p>
-                    </div>
-                </div>
+          <div className="bg-white pt-6 sm:pt-8 lg:pt-12">
+            <div className="mx-auto max-w-screen-2xl px-4 md:px-8">
+              <div className="flex justify-center h-16 mb-6 md:h-32">
+                <img
+                  src={redEclipseLogo}
+                  loading="lazy"
+                  alt="Red Eclipse logo"
+                  className="absolute w-64 object-cover object-center mx-auto md:w-96"
+                />
+              </div>
+              <div className=" my-4 md:my-0">
+                <p className="mx-auto max-w-screen-md text-center text-black md:text-lg">
+                  Bun venit pe RedEclipse - locul unde pasiunile, interesele și
+                  ideile tale se întâlnesc cu o comunitate vibrantă și plină de
+                  energie. Fie că ești aici pentru a-ți conecta cu prietenii și
+                  familia, pentru a-ți extinde rețeaua de contacte sau pentru a
+                  explora comunități și culturi noi, suntem încântați să te avem
+                  alături de noi.{" "}
+                </p>
+              </div>
             </div>
-            <div className="bg-white pb-6 sm:pt-8 lg:pt-12">
-                <div className="mx-auto max-w-screen-2xl px-4 md:px-8">
-                    <h2 className="mb-4 text-center text-2xl font-bold text-gray-800 md:mb-8 lg:text-3xl">Login</h2>
+          </div>
+          <div className="bg-white pb-6 sm:pt-8 lg:pt-12">
+            <div className="mx-auto max-w-screen-2xl px-4 md:px-8">
+              <h2 className="mb-4 text-center text-2xl font-bold text-gray-800 md:mb-8 lg:text-3xl">
+                Login
+              </h2>
 
-                    <form className="red-background mx-auto max-w-lg rounded-lg border-2 border-red-900">
-                        <div className="flex flex-col gap-4 p-4 md:p-8">
-                            <div>
-                                <label htmlFor="email"
-                                       className="mb-2 inline-block text-sm text-gray-800 sm:text-base">Email</label>
-                                <input name="email"
-                                       className="w-full rounded border bg-gray-50 px-3 py-2 text-gray-800 outline-none ring-indigo-300 transition duration-100 focus:ring"/>
-                            </div>
+              <div className="red-background mx-auto max-w-lg rounded-lg border-2 border-red-900">
+                <div className="flex flex-col gap-4 p-4 md:p-8">
+                  <div>
+                    <label className="mb-2 inline-block text-sm text-gray-800 sm:text-base">
+                      Email
+                    </label>
+                    <input
+                      id="email"
+                      type="email"
+                      onChange={handleChange}
+                      name="email"
+                      className="w-full rounded border bg-gray-50 px-3 py-2 text-gray-800 outline-none ring-indigo-300 transition duration-100 focus:ring"
+                    />
+                  </div>
 
-                            <div>
-                                <label htmlFor="password"
-                                       className="mb-2 inline-block text-sm text-gray-800 sm:text-base">Password</label>
-                                <input name="password"
-                                       className="w-full rounded border bg-gray-50 px-3 py-2 text-gray-800 outline-none ring-indigo-300 transition duration-100 focus:ring"/>
-                            </div>
+                  <div>
+                    <label className="mb-2 inline-block text-sm text-gray-800 sm:text-base">
+                      Password
+                    </label>
+                    <input
+                      id="password"
+                      type="password"
+                      onChange={handleChange}
+                      name="password"
+                      className="w-full rounded border bg-gray-50 px-3 py-2 text-gray-800 outline-none ring-indigo-300 transition duration-100 focus:ring"
+                    />
+                  </div>
 
-                            <button
-                                className="block rounded-lg bg-red-900 px-8 py-3 text-center text-sm font-semibold text-white outline-none ring-gray-300 transition duration-100 hover:bg-red-700 focus-visible:ring active:bg-gray-600 md:text-base">Log
-                                in
-                            </button>
-                        </div>
-
-                        <div className="flex items-center justify-center rounded-lg border bg-gray-100 p-4">
-                            <p className="text-center text-sm text-black">Don't have an account? <a href="#"
-                                                                                                       className="text-indigo-500 transition duration-100 hover:text-indigo-600 active:text-indigo-700">Register</a>
-                            </p>
-                        </div>
-                    </form>
+                  <button
+                    onClick={() =>
+                      handleLogin(credentials.email, credentials.password)
+                    }
+                    className="block rounded-lg bg-red-900 px-8 py-3 text-center text-sm font-semibold text-white outline-none ring-gray-300 transition duration-100 hover:bg-red-700 focus-visible:ring active:bg-gray-600 md:text-base"
+                  >
+                    Log in
+                  </button>
                 </div>
+
+                <div className="flex items-center justify-center rounded-lg border bg-gray-100 p-4">
+                  <p className="text-center text-sm text-black">
+                    Don't have an account?{" "}
+                    <a
+                      href="#"
+                      className="text-indigo-500 transition duration-100 hover:text-indigo-600 active:text-indigo-700"
+                    >
+                      Register
+                    </a>
+                  </p>
+                </div>
+              </div>
             </div>
+          </div>
         </div>
-    );
+      ) : (
+        <></>
+      )}
+    </>
+  );
 };
 
 export default Login;
