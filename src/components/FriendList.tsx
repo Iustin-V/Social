99dc60230fe4b7a887cba6d7ef635c306a38c970
Vali <valyparaschiv05@gmail.com>
2023-04-04 18:53:07 +0300

    style

--- src/components/FriendList.tsx
+++ src/components/FriendList.tsx
@@ -1,8 +1,12 @@
+import { Menu } from "@headlessui/react";
 import { fireEvent } from "@testing-library/react";
 import Axios from "axios";
 import React, { useEffect, useState } from "react";
 
-export const FriendList = (props: { setChatData: any }) => {
+export const FriendList = (props: {
+  setChatData?: any;
+  notification?: boolean;
+}) => {
   const [friends, setFriends] = useState([
     { nume: "", prenume: "", poza_profil: "", user_id1: 0, user_id2: 0 },
   ]);
@@ -16,19 +20,43 @@ export const FriendList = (props: { setChatData: any }) => {
       },
     })
       .then((response) => {
-        console.log(response.data);
+        console.log("data_prietenie", response.data);
         setFriends(response.data);
       })
       .catch((error) => {
         console.error(error);
       });
   }, []);
+
+  const friendNotifications = friends.map((friend) => {
+    return (
+      <Menu.Item>
+          <div className="flex flex-row items-center gap-2 justify-between bg-[#e3bbb2] p-2 hover:cursor-pointer hover:bg-red-800 hover:text-white ">
+            <div className="flex flex-row items-center gap-2 justify-start">
+              <img
+              alt={`${friend.nume}${friend.prenume}_poza_profil`}
+              src={`data:image/png;base64,${friend.poza_profil}`}
+              className="shadow-xl rounded-full  h-10 w-10 object-cover  border-none   max-w-150-px"
+            />
+            <div className={"flex flex-row gap-1"}>
+              <div>{friend.nume}</div>
+              <div>{friend.prenume}</div>
+            </div>
+          </div>
+          <div>
+            <button>yes</button>
+            <button>no</button>
+          </div>
+          </div>
+      </Menu.Item>
+    );
+  });
+
   const friendList = friends.map((friend) => {
     return (
       <div
         className="flex flex-row items-center rounded-3xl gap-2 justify-start bg-[#e3bbb2] p-2 hover:cursor-pointer hover:bg-red-800 hover:text-white "
         onClick={() => {
-
           props.setChatData(friend);
         }}
       >
@@ -38,27 +66,31 @@ export const FriendList = (props: { setChatData: any }) => {
           className="shadow-xl rounded-full  h-10 w-10 object-cover  border-none   max-w-150-px"
         />
         <div className={"flex flex-row gap-1"}>
-        <div>{friend.nume}</div>
-        <div>{friend.prenume}</div>
-       </div>
+          <div>{friend.nume}</div>
+          <div>{friend.prenume}</div>
+        </div>
       </div>
     );
   });
   // return <div className="flex flex-col fixed h-screen">{friendList}</div>;
+  if (props.notification) {
+    return <>{friendNotifications}</>;
+  }
   return (
-      <>
-        <div id="docs-sidebar"
-             className="hs-overlay pl-2 sidebar hs-overlay-open:translate-x-0 border-0 -translate-x-full transition-all duration-300 transform fixed bottom-0 left-0 z-20 w-52 bg-white pt-7 pb-10 overflow-y-auto scrollbar-y lg:block lg:translate-x-0 lg:right-auto lg:bottom-0 ">
-          <div className="px-3">
-            <h2 className="flex-none text-2xl font-semibold text-red-800 pb-3"
-               >Friends</h2>
-          </div>
-          <div className="hs-accordion-group w-full flex flex-col flex-wrap" >
-            <ul className="space-y-1.5 flex flex-col gap-2">
-              {friendList}
-            </ul>
-          </div>
+    <>
+      <div
+        id="docs-sidebar"
+        className="hs-overlay pl-2 sidebar hs-overlay-open:translate-x-0 border-0 -translate-x-full transition-all duration-300 transform fixed bottom-0 left-0 z-20 w-52 bg-white pt-7 pb-10 overflow-y-auto scrollbar-y lg:block lg:translate-x-0 lg:right-auto lg:bottom-0 "
+      >
+        <div className="px-3">
+          <h2 className="flex-none text-2xl font-semibold text-red-800 pb-3">
+            Friends
+          </h2>
         </div>
-      </>
-  )
+        <div className="hs-accordion-group w-full flex flex-col flex-wrap">
+          <ul className="space-y-1.5 flex flex-col gap-2">{friendList}</ul>
+        </div>
+      </div>
+    </>
+  );
 };
--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -3,6 +3,7 @@ import { Disclosure, Menu, Transition } from "@headlessui/react";
 import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
 import Axios from "axios";
 import React,{ Fragment, useEffect, useState } from "react";
+import {FriendList} from "./FriendList";
 
 
 
@@ -66,12 +67,12 @@ export const Navigation = () => {
           <div className="relative flex h-16 items-center justify-between">
             <div className="absolute inset-y-0 left-0 flex items-center md:hidden">
               {/* Mobile menu button*/}
-              <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white md:hidden">
+              <Disclosure.Button className="inline-flex items-center justify-center hover:bg-red rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white md:hidden">
                 <span className="sr-only ">Open main menu</span>
                 {false ? (
-                  <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+                  <XMarkIcon className="block h-6 w-6 text-black hover:bg-red" aria-hidden="true" />
                 ) : (
-                  <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+                  <Bars3Icon className="block h-6 w-6 text-black hover:bg-red" aria-hidden="true" />
                 )}
               </Disclosure.Button>
             </div>
@@ -109,17 +110,31 @@ export const Navigation = () => {
               </div>
             </div>
             <div className="absolute inset-y-0 right-0 flex items-center pr-2 md:static md:inset-auto md:ml-6 md:pr-0">
-              <button
-                type="button"
-                className="rounded-full bg-transparent p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 hover-red "
-              >
-                <span className="sr-only">View notifications</span>
-                <BellIcon
-                  className="h-6 w-6 text-white bg-transparent"
-                  aria-hidden="true"
-                />
-              </button>
 
+              <Menu as="div" className="relative ml-3">
+                <div>
+                  <Menu.Button className="flex rounded-full text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                    <span className="sr-only">View notifications</span>
+                    <BellIcon
+                        className="h-6 w-6 text-white bg-transparent"
+                        aria-hidden="true"
+                    />
+                  </Menu.Button>
+                </div>
+                <Transition
+                    as={Fragment}
+                    enter="transition ease-out duration-100"
+                    enterFrom="transform opacity-0 scale-95"
+                    enterTo="transform opacity-100 scale-100"
+                    leave="transition ease-in duration-75"
+                    leaveFrom="transform opacity-100 scale-100"
+                    leaveTo="transform opacity-0 scale-95"
+                >
+                  <Menu.Items className="absolute right-0 z-10 mt-2 w-96 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                    <FriendList notification={true}/>
+                  </Menu.Items>
+                </Transition>
+              </Menu>
               {/* Profile dropdown */}
               <Menu as="div" className="relative ml-3">
                 <div>
