f34a60e9b7a57a55634db910503223da8c6d63e6
Vali <valyparaschiv05@gmail.com>
2023-04-04 14:11:17 +0300

    styling

--- src/pages/Feed.tsx
+++ src/pages/Feed.tsx
@@ -18,7 +18,7 @@ export const Feed = () => {
   const [searchValue, setSearchValue] = useState("");
 
   const handleSearch = (e:any) => {
-    if (e.key === "Enter" && searchValue) {
+    if (e.key === "Enter") {
       Axios.get("http://localhost:3002/api/posts/search", {
         headers: {
           Body: `${searchValue}`,
@@ -67,15 +67,26 @@ export const Feed = () => {
   });
   return (
     <>
-      <div>
-        <input
-          className="p-5 bg-amber-50"
-          onChange={(e) => setSearchValue(e.target.value)}
-          onKeyDown={(e) => handleSearch(e)}
-        />
-      </div>
+
       <div className="feed-wrapper">
-        <div className="feed-column-wrapper">
+        <div className={"max-w-[500px] w-full relative px-6"}>
+          <div className="absolute inset-y-0 left-6 flex items-center pl-3 pointer-events-none">
+            <svg aria-hidden="true" className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor"
+                 viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+              <path fill-rule="evenodd"
+                    d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                    clip-rule="evenodd"></path>
+            </svg>
+          </div>
+          <input
+              type="search"
+              className="p-2 pl-9 w-full rounded-2xl border-b-2 border-red-800 bg-gray-50 focus:border-red-900"
+              placeholder={'Search posts'}
+              onChange={(e) => setSearchValue(e.target.value)}
+              onKeyDown={(e) => handleSearch(e)}
+          />
+        </div>
+        <div className="flex flex-col w-fit md:flex-row">
           <div className="feed-column">
             <CreatePost />
 
